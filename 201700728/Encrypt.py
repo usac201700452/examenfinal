d04df7f6d3c61646219d2ced08c2738a1a18a2f9
usac201602491 <2931416571705@ingenieria.usac.edu.gt>
2020-06-29 08:41:04 -0600

    Fix ecrypt

--- 201700728/Encrypt.py
+++ 201700728/Encrypt.py
@@ -3,17 +3,21 @@ import math
 import os
 from Crypto.Cipher import AES
 
+#CFLN Manejo de encriptado/desencriptado por medio de una clase
 class EncriptarMensaje(object):
     
     def __init__(self):
         pass
 
     def __str__(self):
-        return 'Encriptar mensajes'
+        return 'Encriptacion/Desencriptacion de mensajes'
 
     def __repr__(self):
         return self.__str__()
 
+    #CFLN Metodo para encriptar mensajes mediante generacion
+    #del algoritmo hash sha256 y mediante el uso de AES
+    #y una contraseña convertida a binario.
     def encriptar(self,mensaje):
         
         self.mensaje = mensaje
@@ -23,10 +27,9 @@ class EncriptarMensaje(object):
         SALT_SIZE = 16 # Tamaño arbitrario
         password = b'contrasenia altamente segura'       
 
-        if type(mensaje)!=bytes:
+        if type(mensaje)!=bytes:                #CFLN si el mensaje no es binario se convierte
             cleartext = mensaje.encode()
-
-        else:
+        else:                                   #CFLN si el mensaje ya es binario simplemente se toma su valor
             cleartext = mensaje
         
         salt = os.urandom(SALT_SIZE)
@@ -37,8 +40,11 @@ class EncriptarMensaje(object):
 
         encriptado = salt + AES.new(key, AES.MODE_CFB, iv).encrypt(cleartext)
 
-        return encriptado
+        return encriptado           
+        #CFLN El mensaje encritado se devuelve en hexadecimal binario
 
+    #CFLN Metodo para desencriptar el mensaje que se recibe en binario
+    #utilizando el algoritmo de hash sha256 y el uso de AES
     def desencriptar(self, mensaje_enc):
         self. mensaje_enc = mensaje_enc
 
@@ -56,3 +62,4 @@ class EncriptarMensaje(object):
         desencriptado = AES.new(key, AES.MODE_CFB, iv).decrypt(mensaje_enc[SALT_SIZE:])
 
         return desencriptado
+        #El mensaje encriptado se devuelve en string binario
